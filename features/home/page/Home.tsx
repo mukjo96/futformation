66947bd48333788961c2cb4a9501e476a9bd429a
mukjo96 <mukjo96@gmail.com>
2021-08-21 18:52:25 +0900

    fix: Make AdBanner invisible

--- features/home/page/Home.tsx
+++ features/home/page/Home.tsx
@@ -73,7 +73,7 @@ const Home = () => {
                     color={team.teamColor}
                     teamId={team.teamId}
                 />
-                <AdBanner teamColor={team.teamColor} />
+                {/* <AdBanner teamColor={team.teamColor} /> */}
                 {/* <Sponsorship /> */}
                 <BackDiv></BackDiv>
             </Fragment>
